a8065c155fc0d5f6f6f47d16c244fcb7436ee181
Ron Oh <ronoh2001@gmail.com>
2015-12-13 15:48:27 -0800

    table and map stuff

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function initMap() {
   var CodeFellows = {lat: 47.6235481, lng: -122.33621199999999};
   map = new google.maps.Map(document.getElementById('map'), {
     center: CodeFellows,
-    zoom: 16
+    zoom: 15
   });
   var marker=new google.maps.Marker({
     position:CodeFellows,
@@ -21,7 +21,8 @@ function initMap() {
     radius: 500,
     types: ['restaurant', 'cafe','bakery', 'meal_takeaway', 'food', 'point_of_interest','bar'],
     openNow: true,
-    // Can add maxPrice: 1,2,3,etc. via the 'submit' event listener?
+    maxPriceLevel: 1
+    // Can add maxPrice: 1,2,3,etc. via the 'submit' event listener? MaxPriceLevel:1-4
   }, callback);
 }
 
@@ -29,7 +30,29 @@ function callback(results, status) {
   if (status === google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < results.length; i++) {
       createMarker(results[i]);
-      console.log(results[i]);
+      
+     //  var $search = $('#searchResults');
+     //  // console.log('---', $search);
+     // $search.append("<tr id='name'>")
+     // $('#name').append('<td id="rest">');
+     //  // var local = [],
+     // for (var j= 0; j<results.length; j++) {
+     //  $('.restaurant').append(results[i].name);
+     // }
+  results.forEach(function(result) {
+        $('.restaurants').append(results[i].name);
+      })
+    $('#rest' + results[i + 1] + '').results[i].name;
+  console.log(results[i].name);
+
+      // results.forEach(function(result) {
+      //   $('.restaurants').append(result.name[i]);
+        // $('#rest1').append(result.name[0]);
+        // console.log('---', results[i]);
+         
+     //      // $search.append.(<td>)(result.name)
+     //  });
+
       // TODO: use the results information to populate form
     }
   }
@@ -41,7 +64,14 @@ function createMarker(place) {
     position: place.geometry.location
   });
   google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent(place.name);
+    var nameLocation = place.name + ' ' +place.vicinity + " Rated" + place.rating; 
+    infowindow.setContent(nameLocation);
+    // infowindow.setContent(place.vicinity);
     infowindow.open(map, this);
   });
 }
+//   google.maps.event.addListener(marker, 'click', function() {
+//     infowindow.setContent(place.name);
+//     infowindow.open(map, this);
+//   });
+// }
